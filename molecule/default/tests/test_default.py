3c37953ea8166d1ba7abc3b8014baae8c7428284
Ted Cook <teodoro.cook@gmail.com>
2017-09-21 15:45:11 -0600

    Refactor tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,16 +5,25 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+user_name = 'sensu'
+user_group = 'sensu'
+user_home = '/opt/sensu'
+user_rbenv = '{0}/.rbenv'.format(user_home)
+ruby_version = '2.4.1'
+ruby_bin = '{0}/versions/{1}/bin/ruby'.format(user_rbenv, ruby_version)
+
 
 def test_user(host):
-    assert 'sensu' in host.user('sensu').groups
-    assert host.user('sensu').home == "/opt/sensu"
-    assert host.file('/opt/sensu').exists
-    assert host.file('/opt/sensu').is_directory
+    assert user_group in host.user(user_name).groups
+    assert host.user(user_name).home == user_home
+    assert host.file(user_home).exists
+    assert host.file(user_home).is_directory
 
 
 def test_rbenv(host):
-    assert host.file('/opt/sensu/.rbenv').exists
-    assert host.file('/opt/sensu/.rbenv').is_directory
-    assert host.file('/opt/sensu/.rbenv/2.4.1/bin/ruby').exists
-    assert host.file('/opt/sensu/.rbenv/2.4.1/bin/ruby').is_file
+    assert host.file(user_rbenv).exists
+    assert host.file(user_rbenv).is_directory
+    assert host.file(ruby_bin).exists
+    assert host.file(ruby_bin).is_file
+    with host.sudo(user_name):
+        host.run('{0} --version'.format(ruby_bin)).rc == 0
